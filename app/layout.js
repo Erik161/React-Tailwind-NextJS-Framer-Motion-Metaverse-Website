be179eec48b615517295f5f391645537f9423ea5
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-12-04 22:40:45 -0600

    add file App

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css';
+
+const RootLayout = ({children})=> (
+
+    <html lang="en">
+        
+        <head>
+            <link rel="preconnect" href="https://www.behance.net/erikhernandez161" />
+            <link rel="preconnect" href="https://github.com/Erik161" />
+        </head>
+
+
+    </html>
+
+
+);

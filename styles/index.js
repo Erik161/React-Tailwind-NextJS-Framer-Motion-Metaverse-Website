e11518235e919de164b068791a7c1220d295cfe1
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-12-15 16:05:10 -0600

    styles/index.js | Layout

--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,5 @@
 const styles ={
-    innerWidth: '2xl:max-w[1280px] w-full',
+    innerWidth: '2xl:max-w-[1280px] w-full',
     interWidth:'lg:w-[80%] w-[100%]',
 
     paddings:'sm:p-16 xs:p-8 px-6 py-12',
@@ -9,7 +9,7 @@ const styles ={
     bottomPaddings:'sm:pb-16 xs:pb-8 pb-12',
 
     flexCenter:'flex justify-center items-center',
-    flexStart:'flex justify-start items-center',
+    flexStart:'flex justify-start items-start',
     flexEnd:'flex justify-end',
     navPadding:'pt-[98px]',
 

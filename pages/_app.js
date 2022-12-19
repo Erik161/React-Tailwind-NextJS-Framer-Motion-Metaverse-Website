535575e22844e1fff1eccce6cabe3c3489c767ba
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-12-18 19:25:30 -0600

    Pages/_app.js | LAYOUT

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
+
+const MyApp = ({Component, pageProps})=>(
+  <>
+    <Head>
+      <title>Metaversus</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://github.com/Erik161'  />
+        <link rel='stylesheet' href='https://www.linkedin.com/in/erikhern%C3%A1ndez161/' />
+      </meta>
+    </Head>
+    <Component {...pageProps}/>
+  </>
+);
+
+
+export default MyApp;
